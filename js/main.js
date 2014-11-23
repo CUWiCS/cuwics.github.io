5754d4669982d06c011cfac07e824797a0b0b780
chicken <cc3636@columbia.edu>
2014-11-23 02:22:06 -0500

    added title font and fixed some margins

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function(n){n.viewportSize={},n.viewportSize.getHeight=function(){return t("Height")},n.viewportSize.getWidth=function(){return t("Width")};var t=function(t){var f,o=t.toLowerCase(),e=n.document,i=e.documentElement,r,u;return n["inner"+t]===undefined?f=i["client"+t]:n["inner"+t]!=i["client"+t]?(r=e.createElement("body"),r.id="vpw-test-b",r.style.cssText="overflow:scroll",u=e.createElement("div"),u.id="vpw-test-d",u.style.cssText="position:absolute;top:-1000px",u.innerHTML="<style>@media("+o+":"+i["client"+t]+"px){body#vpw-test-b div#vpw-test-d{"+o+":7px!important}}<\/style>",r.appendChild(u),i.insertBefore(r,e.head),f=u["offset"+t]==7?i["client"+t]:n["inner"+t],i.removeChild(r)):f=n["inner"+t],f}})(this);
 
 //look this up!
-$(document).ready(function(){
+/*$(document).ready(function(){
 
 	$(".main").onepage_scroll({
 	   sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	   updateURL: false // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 	});
 	
-});
+});*/
 
 /**
  * How to create a parallax scrolling website
