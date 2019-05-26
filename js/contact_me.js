2c7523914035c268ef088c730651c8745caea4ca
Annie Sui <46787174+breadou-sui@users.noreply.github.com>
2019-05-26 19:39:30 -0400

    updated contactme take 1

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,6 +46,15 @@ $(function() {
                     Message: message,
                     _cc: ""
                 },
+                
+            $.post('https://mailthis.to/test@example.com', {
+              email: 'foo@bar.co',
+              _subject: 'hi!',
+              message: 'Test'
+            }).then(function () {
+              location.href = 'https://mailthis.to/confirm'
+            });
+            
                 cache: false,
                 success: function(event) {
                     console.log(event);
